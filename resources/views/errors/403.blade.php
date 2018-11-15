76f28a19d6da5a736f97f9b2a5a373d71fa84570
codename11 <veljkos82@gmail.com>
2018-11-15 19:52:53 +0100

    Added custom page for policy authorization. Just a try though, but works.

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'LSAPP') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+
+    <!--Custom strana za ono kad se brani pristup korisniku preko polisa.-->
+        <img src="https://vignette.wikia.nocookie.net/dragonology/images/2/22/Death_Dragon.jpg/revision/latest?cb=20150427140253" style="width:100%;">
+    
+</body>
+</html>
