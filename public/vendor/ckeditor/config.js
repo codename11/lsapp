bc9b09179fbf48fc03cc8cc054044f9a77c8df69
codename11 <veljkos82@gmail.com>
2018-11-27 20:21:26 +0100

    Figured out how to add new smileys to ckeditor

--- public/vendor/ckeditor/config.js
+++ public/vendor/ckeditor/config.js
@@ -7,4 +7,8 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.smiley_images = [
+		'lisa.gif', 'angel_smile.gif', 'angry_smile.gif', 'broken_heart.gif', 'confused_smile.gif', 'cry_smile.gif', 'devil_smile.gif', 'embaressed_smile.gif', 'embarrassed_smile.gif', 'envelope.gif', 'heart.gif', 'kiss.gif', 'lightbulb.gif', 'omg_smile.gif', 'regular_smile.gif', 'sad_smile.gif', 'shades_smile.gif', 'teeth_smile.gif', 'thumbs_down.gif', 'thumbs_up.gif', 'tongue_smile.gif', 'tounge_smile.gif', 'whatchutalkingabout_smile.gif', 'wink_smile.gif', 'lisa1.png'
+	];
+	
 };
