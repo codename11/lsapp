9418e8721db1d9529694846a23da4785460cce33
codename11 <veljkos82@gmail.com>
2018-12-20 23:40:33 +0100

    Under every post now is button to convert it to pdf..

--- resources/views/pdfview.blade.php
+++
@@ -1,9 +0,0 @@
-@extends("layouts.app")
-<!--Ovde se navodi iz kog se direktorijuma uzima template. -->
-
-@section("content")
-
-    <h1>ttt</h1>
-
-@endsection
-<!--Ovo oznacava sta sve obuhvata template. -->
